ade0f05de497a0e4f71a1382a1c953b877b90aee
yourusername <ang25001@byui.edu>
2026-01-17 20:00:25 -0700

    Add DOM theme files for week 2

--- mission/script.js
+++ mission/script.js
@@ -7,9 +7,9 @@ themeSelect.addEventListener("change", function () {
 
   if (theme === "dark") {
     body.classList.add("dark");
-    logo.src = "images/byui-logo-white.png";   // your dark-mode logo
+    logo.src = "images/byui-logo-white.png";   // white logo for dark mode
   } else {
     body.classList.remove("dark");
-    logo.src = "images/byui-logo-blue.webp";  // your light-mode logo
+    logo.src = "images/byui-logo-blue.webp";   // blue logo for light mode
   }
 });
